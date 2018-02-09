12c540dc8dc71be7eda9489f83fadaec030727a2
Pzoom <admin@pzoomspace.cn>
2018-02-09 22:44:26 +0800

    居然忘记删除密钥了！ :worried:

--- twitterSpider/twitter.py
+++ twitterSpider/twitter.py
@@ -3,10 +3,10 @@ from TwitterAPI import TwitterAPI
 import json
 
 result=open("..realtimeSample.csv",'w')
-consumer_key="gBBbFtbfCATXQcHpEwr6d9azm"
-consumer_secret="tF9A03v5EcErAWeohNNZI6BMVhmv5Zc4OSM4gs8G2E2HodJyBC"
-access_token_key="961809210349535232-zb6BjSjwBcB6y9uPOsxv37WrjOl7jE3"
-access_token_secret="lf1Yxp2OHVQVZYfNEDg3rT4z7jBZWuwIOUt2xYzcEEuEk"
+consumer_key=""
+consumer_secret=""
+access_token_key=""
+access_token_secret=""
 api = TwitterAPI(consumer_key, consumer_secret, access_token_key, access_token_secret)
 
 r = api.request('statuses/sample', {})#sample randomly(offical)
